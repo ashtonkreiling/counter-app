050860f3fa86081a22c7ce6bc346df9a369be4cd
ashtonkreiling <ashtonkreiling@gmail.com>
2023-03-06 20:49:30 -0700

    Make Counters Work

---
+++ src/components/counter.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    state = {
+        value: this.props.value,
+    };
+
+    handleIncrement = () => {
+        this.setState({value: this.state.value + 1});
+    }
+
+    render() {
+        console.log(this.props.value);
+        return (
+        <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button>
+        </div>
+        )
+    };
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-"
+        classes += (this.state.value === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    formatCount() {
+        const {value: value} = this.state;
+        return value === 0 ? 'Zero' : value
+    }
+}
+ 
+export default Counter;
